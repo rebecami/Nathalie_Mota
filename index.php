b6d54b6c7285e5510c7146bcaaa289714c5b56fe
rebecami <161315006+rebecami@users.noreply.github.com>
2024-07-02 14:30:56 +0200

    Hero creation
    Intégration du hero avec une image aléatoire du catalogue

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php get_header() ?>
 
-
+<!-- HERO -->
+<div class="hero">
+    <h1 class="site_title"> Photographe event</h1>
+        <?php
+        $query_hero = new WP_Query([
+            'post_type' => 'photo',        
+            'posts_per_page' => 1,
+            'tax_query' =>[
+                [
+                    'taxonomy' => 'format',
+                    'field' => 'slug',
+                    'terms' => 'paysage'
+                ]
+            ]
+        ]);
+            while($query_hero->have_posts()){ 
+                $query_hero->the_post();?>
+                    <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                    
+            <?php
+            } 
+        ?>    
+</div>
 
 <?php get_footer() ?>
